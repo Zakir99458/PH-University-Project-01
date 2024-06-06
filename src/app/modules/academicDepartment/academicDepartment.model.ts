20e1c9b167a76d7e191a9ac485020b98ef989fd6
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-06 13:44:12 +0300

    middlewareUsed-to-check

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -17,6 +17,18 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 )
 
+// Check for existence of Department
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  })
+  if (isDepartmentExist) {
+    throw new Error('Department is already exists..')
+  }
+
+  next()
+})
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -2,13 +2,6 @@ import { TAcademicDepartment } from './academicDepartment.interface'
 import { AcademicDepartment } from './academicDepartment.model'
 
 const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
-  // Check for existence of Department
-  const isDepartmentExist = await AcademicDepartment.findOne({
-    name: payload.name,
-  })
-  if (isDepartmentExist) {
-    throw new Error('Department is already exists..')
-  }
   const result = await AcademicDepartment.create(payload)
 
   return result
