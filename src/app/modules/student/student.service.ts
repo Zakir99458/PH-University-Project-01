3623c32d95abcc71cebadd709867ede1b8b2d431
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-06 20:15:43 +0300

    user-student-deleted

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose'
 import { Student } from './student.model'
+import AppError from '../../errors/AppErrors'
+import httpStatus from 'http-status'
+import { User } from '../user/user.model'
 
 const getAllStudentsFromDB = async () => {
   const result = await Student.find()
@@ -11,8 +15,35 @@ const getSingleStudentsFromDB = async (id: string) => {
 }
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true })
-  return result
+  const session = await mongoose.startSession()
+
+  try {
+    session.startTransaction()
+    const deleteStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    )
+    if (!deleteStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Unable to delete Student')
+    }
+
+    const deleteUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    )
+    if (!deleteUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Unable to delete User')
+    }
+    await session.commitTransaction()
+    await session.endSession()
+    return deleteStudent
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete!!')
+  }
 }
 
 export const StudentServices = {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import { AcademicSemester } from '../academicSemester/academiSemester.model'
 import { TStudent } from '../student/student.interface'
@@ -5,6 +6,8 @@ import { Student } from '../student/student.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 import { generateStudentId } from './user.utils'
+import AppError from '../../errors/AppErrors'
+import httpStatus from 'http-status'
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   //   if (await student.isUserExists(studentData.id)) {
@@ -22,17 +25,34 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   )
 
-  // Set ID for student, embedded
-  userData.id = await generateStudentId(admissionSemester)
-  // create user
-  const newUser = await User.create(userData)
+  const session = await mongoose.startSession()
 
-  // create student
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id // embedding ID
-    payload.user = newUser._id // Reference ID
-    const newStudent = Student.create(payload)
+  try {
+    ;(await session).startTransaction()
+    // Set ID for student, embedded
+    userData.id = await generateStudentId(admissionSemester)
+    // create user
+    const newUser = await User.create([userData], { session })
+
+    // create student
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Unable to create new user!')
+    }
+    payload.id = newUser[0].id // embedding ID
+    payload.user = newUser[0]._id // Reference ID
+    const newStudent = Student.create([payload], { session })
+    if (!(await newStudent).length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Unable to create new student')
+    }
+
+    ;(await session).commitTransaction()
+    ;(await session).endSession()
+
     return newStudent
+  } catch (err) {
+    ;(await session).abortTransaction()
+    ;(await session).endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, 'Unable Do the Transaction')
   }
 }
 
