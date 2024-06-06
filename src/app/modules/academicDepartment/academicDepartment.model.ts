598f53adbb653d30de54e5b1e1090a1b292db7fa
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-06 16:13:04 +0300

    appEror-file-added

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose'
 import { TAcademicDepartment } from './academicDepartment.interface'
+import AppError from '../../errors/AppErrors'
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   {
@@ -23,7 +24,7 @@ academicDepartmentSchema.pre('save', async function (next) {
     name: this.name,
   })
   if (isDepartmentExist) {
-    throw new Error('Department is already exists..')
+    throw new AppError(404, 'Department is already exists..')
   }
 
   next()
@@ -34,7 +35,7 @@ academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const isDepartmentExist = await AcademicDepartment.findOne(query)
 
   if (!isDepartmentExist) {
-    throw new Error('Department does not exit')
+    throw new AppError(404, 'Department does not exit')
   }
   next()
 })
