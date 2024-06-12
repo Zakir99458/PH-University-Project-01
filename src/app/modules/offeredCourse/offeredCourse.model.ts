bd8dddc6b76a7f3acbcaf7cb9f1d1071d7cd4636
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-12 11:02:09 +0300

    file-mis-match-erro-correc

--- src/app/modules/offeredCourse/offeredCourse.model.ts
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
-import { Days } from './OfferedCourse.constant'
-import { TOfferedCourse } from './OfferedCourse.interface'
+import { TOfferedCourse } from './offeredCourse.interface'
+import { Days } from './offeredCourse.constant'
 
 const offeredCourseSchema = new mongoose.Schema<TOfferedCourse>(
   {
