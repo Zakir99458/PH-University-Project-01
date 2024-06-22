295b6c3823f26c0758b6f5421aec9f9bdb4f8785
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-22 21:54:36 +0300

    enrolled-course-module-added

--- src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
@@ -4,13 +4,17 @@ import sendResponse from '../../utils/sendResponse'
 import { EnrolledCourseServices } from './enrolledCourse.service'
 
 const createEnrolledCourse = catchAync(async (req, res) => {
-  const result = await EnrolledCourseServices.createEnrolledCoureseIntoDB()
+  const userId = req.user.jwtPayload.userId
+  const result = await EnrolledCourseServices.createEnrolledCourseIntoDB(
+    userId,
+    req.body,
+  )
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Student is enrolled successfully',
-    data: result,
+    data: {},
   })
 })
 
--- src/app/modules/EnrolledCourse/enrolledCourse.route.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.route.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import { EnrolledCourseValidations } from './enrolledCourse.validation'
 import { EnrolledCourseControllers } from './enrolledCourse.controller'
 import validateRequest from '../../middlewares/validateRequest'
+import auth from '../../middlewares/auth'
 
 const router = express.Router()
 
 router.post(
-  '/create-course',
+  '/create-enrolled-course',
+  auth('student'),
   validateRequest(
     EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
   ),
